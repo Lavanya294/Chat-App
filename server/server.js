4383dcdce55af0b1ca23b6ba32bc42e965e130be
Lavanya Gupta <lavanyagupta294@gmail.com>
2025-08-26 22:50:49 +0530

    server.jsx and app.jsx updated

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 //Initialize socket.io server (must attach to server)
 export const io = new Server(server, {
-    cors: { origin: "*" }
+  cors: { origin: "*" },
 });
 
 //Store online users
@@ -21,19 +21,19 @@ export const userSocketMap = {}; // {userId : socketId}
 
 //Socket.io connection handler
 io.on("connection", (socket) => {
-    const userId = socket.handshake.query.userId;
-    console.log("User Connected:", userId);
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected:", userId);
 
-    if (userId) userSocketMap[userId] = socket.id;
+  if (userId) userSocketMap[userId] = socket.id;
 
-    //Emit online users to all connected clients
+  //Emit online users to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", userId);
+    delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-    socket.on("disconnect", () => {
-        console.log("User Disconnected:", userId);
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
+  });
 });
 
 //Middleware setup
@@ -48,6 +48,10 @@ app.use("/api/messages", messageRouter);
 //Connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+}
 
-server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+// Export server for vercel
+export default server;
